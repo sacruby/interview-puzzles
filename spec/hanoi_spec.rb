506e5f05cc54caea7868e689af5f5367c4c718c8
Christopher Haupt <chaupt@mobirobo.com>
2013-04-18 16:12:09 -0700

    Fix Hanoi Tests

--- spec/hanoi_spec.rb
+++ spec/hanoi_spec.rb
@@ -1,42 +1,44 @@
 describe "towers of hanoi" do
 
   before(:each) do
-    stack1 = Stack.new
-    stack2 = Stack.new
-    stack3 = Stack.new
+    @stack1 = Stack.new
+    @stack2 = Stack.new
+    @stack3 = Stack.new
 
-    stack1.push(3)
-    stack1.push(2)
-    stack1.push(1)
+    @stack1.push(3)
+    @stack1.push(2)
+    @stack1.push(1)
+
+    @game = Hanoi.new(@stack1, @stack2, @stack3)
   end
 
   it "should win the game" do
-    play(stack1, stack2, stack3)
-    stack1.size.should == 0
-    stack2.size.should == 0
-    stack3.pop.should == 1
-    stack3.pop.should == 2
-    stack3.pop.should == 3
+    @game.play
+    @stack1.size.should == 0
+    @stack2.size.should == 0
+    @stack3.pop.should == 1
+    @stack3.pop.should == 2
+    @stack3.pop.should == 3
   end
 
   it "should make first move" do
-    step(stack1, stack2, stack3)
-    stack1.size.should == 2
-    stack2.size.should == 0
-    stack3.size.should == 1
-    stack1.peek.should == 2
-    stack3.peek.should == 1
+    @game.step
+    @stack1.size.should == 2
+    @stack2.size.should == 0
+    @stack3.size.should == 1
+    @stack1.peek.should == 2
+    @stack3.peek.should == 1
   end
 
   it "should make second move" do
-    step(stack1, stack2, stack3)
-    step(stack1, stack2, stack3)
-    stack1.size.should == 1
-    stack2.size.should == 1
-    stack3.size.should == 1
-    stack1.peek.should == 3
-    stack2.peek.should == 2
-    stack3.peek.should == 1
+    @game.step
+    @game.step
+    @stack1.size.should == 1
+    @stack2.size.should == 1
+    @stack3.size.should == 1
+    @stack1.peek.should == 3
+    @stack2.peek.should == 2
+    @stack3.peek.should == 1
   end
 
 end
